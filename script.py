53d555dfe9e43e5b40ba6b76643f5d6fb03b7127
Nicola Cammillini <nicola.cammillini@gmail.com>
2017-09-17 10:30:32 +0200

    Basic starting script

---
+++ script.py
@@ -0,0 +1,32 @@
+import csv
+
+csv.register_dialect('pipe_dialect', delimiter='|')
+
+errors = []
+
+try:
+    with open('inputfile.csv') as incsvfile, open('outputfile.csv' ,'w') as outcsvfile:
+
+
+        csvreader = csv.reader(incsvfile, 'pipe_dialect')
+        csvwriter = csv.writer(outcsvfile, delimiter=',')
+
+        for inputline in csvreader:
+
+            # dummy computation on a line of input:
+            # just writing same line with different format
+            # Please note computation is made on lists: no
+            # dependencies on input-output format! That is
+            # an implementation detail.
+            outputline = inputline
+
+            csvwriter.writerow(outputline)
+except csv.Error as csveo:
+    errors.append(csveo)
+except IOError as ioe:
+    errors.append(ioe)
+finally:
+    print('Errors encountered...')
+    print(errors)
+
+csv.unregister_dialect('pipe_dialect')
