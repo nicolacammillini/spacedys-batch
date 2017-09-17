faa4aeb77dd9c96cbec0efd468bcb766eae8e6e1
Nicola Cammillini <nicola.cammillini@gmail.com>
2017-09-17 13:58:32 +0200

    Driving Function made into template method

--- script.py
+++ script.py
@@ -1,8 +1,38 @@
 from fileformats import get_reader_for_file, get_writer_for_file, FileFormatException
 
+class FileBatch:
 
-def do_computation(inputline):
-    return inputline
+    def do_computation(self, inputline):
+        return inputline
+
+    def process(self, infile, outfile):
+
+        try:
+            with open(infile) as inputfile, open(outfile, 'w') as outputfile:
+    
+                reader = get_reader_for_file(inputfile)
+                writer = get_writer_for_file(outputfile)
+    
+                for inputline in reader:
+    
+                    # dummy computation on a line of input:
+                    # just writing same line with different format
+                    # Please note computation is made on lists: no
+                    # dependencies on input-output format! That is
+                    # an implementation detail.
+                    outputline = self.do_computation(inputline)
+    
+                    writer.writeline(outputline)
+    
+        except FileFormatException as ffe:
+            errors.append(ffe)
+        except IOError as ioe:
+            errors.append(ioe)
+        finally:
+            print('Errors encountered...')
+            print(errors)
+
+
 
 
 errors = []
@@ -10,32 +40,6 @@ errors = []
 INPUTFILE = 'inputfile.csv'
 OUTPUTFILE = 'outputfile.csv'
 
-def process_batch(infile, outfile):
+batch = FileBatch()
 
-    try:
-        with open(infile) as inputfile, open(outfile, 'w') as outputfile:
-
-            reader = get_reader_for_file(inputfile)
-            writer = get_writer_for_file(outputfile)
-
-            for inputline in reader:
-
-                # dummy computation on a line of input:
-                # just writing same line with different format
-                # Please note computation is made on lists: no
-                # dependencies on input-output format! That is
-                # an implementation detail.
-                outputline = do_computation(inputline)
-
-                writer.writeline(outputline)
-
-    except FileFormatException as ffe:
-        errors.append(ffe)
-    except IOError as ioe:
-        errors.append(ioe)
-    finally:
-        print('Errors encountered...')
-        print(errors)
-
-
-process_batch(INPUTFILE, OUTPUTFILE)
+batch.process(INPUTFILE, OUTPUTFILE)
